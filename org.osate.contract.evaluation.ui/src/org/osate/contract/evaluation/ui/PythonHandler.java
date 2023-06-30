b449f45e0e735efbe158853c181a7a8339572133
Lutz Wrage <lutz.wrage@gmail.com>
2023-06-30 15:36:24 -0400

    show console output in an eclipse console view

--- org.osate.contract.evaluation.ui/src/org/osate/contract/evaluation/ui/PythonHandler.java
+++ org.osate.contract.evaluation.ui/src/org/osate/contract/evaluation/ui/PythonHandler.java
@@ -1,29 +1,32 @@
 /*******************************************************************************
  * Assurance Contract Annex Plugin for OSATE
  * Copyright 2023 Carnegie Mellon University.
- * NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE 
- * MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO 
- * WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT 
- * NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR 
- * RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE 
- * ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT 
+ * NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE
+ * MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO
+ * WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT
+ * NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR
+ * RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE
+ * ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT
  * INFRINGEMENT.
- * Released under a BSD (SEI)-style license, please see license.txt or contact 
+ * Released under a BSD (SEI)-style license, please see license.txt or contact
  * permission@sei.cmu.edu for full terms.
- * [DISTRIBUTION STATEMENT A] This material has been approved for public release and 
- * unlimited distribution.  Please see Copyright notice for non-US Government use and 
+ * [DISTRIBUTION STATEMENT A] This material has been approved for public release and
+ * unlimited distribution.  Please see Copyright notice for non-US Government use and
  * distribution.
- * Carnegie Mellon® is registered in the U.S. Patent and Trademark Office by Carnegie 
+ * Carnegie Mellon® is registered in the U.S. Patent and Trademark Office by Carnegie
  * Mellon University.
- * This Software includes and/or makes use of the following Third-Party Software subject 
+ * This Software includes and/or makes use of the following Third-Party Software subject
  * to its own license:
- * 1. Z3 (https://github.com/Z3Prover/z3/blob/master/LICENSE.txt) Copyright Microsoft 
+ * 1. Z3 (https://github.com/Z3Prover/z3/blob/master/LICENSE.txt) Copyright Microsoft
  * Corporation.
- * 2. Eclipse (https://www.eclipse.org/legal/epl-2.0/) Copyright 2000, 2023 Eclipse 
+ * 2. Eclipse (https://www.eclipse.org/legal/epl-2.0/) Copyright 2000, 2023 Eclipse
  * contributors and others.
  * DM23-0575
  *******************************************************************************/
 package org.osate.contract.evaluation.ui;
+
+import java.io.PrintStream;
+import java.util.Arrays;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -31,7 +34,12 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.ease.service.IScriptService;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.osate.aadl2.instance.ComponentInstance;
 import org.osate.aadl2.instance.SystemInstance;
@@ -57,8 +65,40 @@ public class PythonHandler extends AbstractHandler {
 		var processor = new ContractProcessor(component,
 				scriptService.getEngineByID("org.eclipse.ease.lang.python.py4j.engine"));
 //		processor.processContract(component);
+		linkConsole(event);
 		processor.processVerificationPlan(component);
+		unlinkConsole();
 		return null;
 	}
 
+	private static final String CONTRACT_CONSOLE = "Contract Console";
+
+	private static PrintStream out;
+
+	private static void linkConsole(ExecutionEvent event) {
+		try {
+			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView(IConsoleConstants.ID_CONSOLE_VIEW);
+		} catch (PartInitException e) {
+			ConsolePlugin.log(e);
+		}
+		var consoleMgr = ConsolePlugin.getDefault().getConsoleManager();
+		var console = Arrays.stream(consoleMgr.getConsoles())
+				.filter(con -> con.getName().equals(CONTRACT_CONSOLE) && con instanceof MessageConsole)
+				.map(MessageConsole.class::cast)
+				.findFirst()
+				.orElseGet(() -> {
+					var con = new MessageConsole(CONTRACT_CONSOLE, null);
+					consoleMgr.addConsoles(new IConsole[] { con });
+					return con;
+				});
+		out = new PrintStream(console.newMessageStream());
+		System.setOut(out);
+		System.setErr(out);
+	}
+
+	private static void unlinkConsole() {
+		System.setOut(System.out);
+		System.setErr(System.err);
+		out.close();
+	}
 }
