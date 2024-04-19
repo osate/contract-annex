92c90eb8f3ab3fd3e1ae925473702c27347a3ca7
dionisio <dionisio@sei.cmu.edu>
2024-04-19 10:05:38 -0400

    Added a URI handler to be called from the svg interface and go to a problem marker.

---
+++ org.osate.contract/src/org/osate/contract/uriHandler/GSNURIMarkerCommandHandler.java
@@ -0,0 +1,43 @@
+package org.osate.contract.uriHandler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.views.markers.MarkerViewUtil;
+
+public class GSNURIMarkerCommandHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		var<String, String> set = event.getParameters();
+		String analysisName = (String) set.get("org.osate.contract.uriHandler.GSNURIMarker.analysisName");
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		IWorkbenchPage page = window.getActivePage();
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IResource resource = workspace.getRoot();
+		IMarker[] markers;
+		try {
+			markers = resource.findMarkers(IMarker.MARKER, true, IResource.DEPTH_INFINITE);
+			for (IMarker m : markers) {
+				String message = (String) m.getAttribute(IMarker.MESSAGE);
+				if (message != null && message.contains(analysisName)) {
+					MarkerViewUtil.showMarker(page, m, true);
+					break;
+				}
+			}
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
