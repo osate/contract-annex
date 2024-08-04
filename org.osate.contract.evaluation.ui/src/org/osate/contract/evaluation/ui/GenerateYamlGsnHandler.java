680f09da8d52e27e4f8efc0c8997b87d85440eaf
dionisio <dionisio@sei.cmu.edu>
2024-08-04 08:08:36 -0400

    Added a simple execution interface for the gsn2x in Linux.

--- org.osate.contract.evaluation.ui/src/org/osate/contract/evaluation/ui/GenerateYamlGsnHandler.java
+++ org.osate.contract.evaluation.ui/src/org/osate/contract/evaluation/ui/GenerateYamlGsnHandler.java
@@ -1,6 +1,9 @@
 package org.osate.contract.evaluation.ui;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -24,6 +27,7 @@ import org.osate.contract.gsn.YamlFolder;
 import org.osate.contract.gsn.YamlGsnGenerator;
 
 public class GenerateYamlGsnHandler extends AbstractHandler {
+	String pathtoYaml = "";
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		var node = (EObjectNode) HandlerUtil.getCurrentStructuredSelection(event).getFirstElement();
@@ -34,6 +38,7 @@ public class GenerateYamlGsnHandler extends AbstractHandler {
 		});
 		var yamlFolder = generatedYaml.folder();
 		var folderPath = generatedYaml.project().getFullPath().append("yaml-gen").append(yamlFolder.name());
+		pathtoYaml = "";
 		try {
 			HandlerUtil.getActiveWorkbenchWindow(event).run(true, true, new WorkspaceModifyOperation() {
 				@Override
@@ -41,8 +46,16 @@ public class GenerateYamlGsnHandler extends AbstractHandler {
 						throws CoreException, InvocationTargetException, InterruptedException {
 					var subMonitor = SubMonitor.convert(monitor, "Generating YAML-GSN", yamlFolder.files().size() + 1);
 					var folder = new ContainerGenerator(folderPath).generateContainer(subMonitor.split(1));
+
 					for (var yamlFile : yamlFolder.files()) {
 						var file = folder.getFile(new Path(yamlFile.name()));
+
+						// Dio: for temporal the yaml compilation
+						if (pathtoYaml.length() == 0) {
+							pathtoYaml = file.getParent().getRawLocation().toOSString();
+						}
+						// Dio: end of yaml compilation piece
+
 						var stream = new ByteArrayInputStream(yamlFile.contents().getBytes());
 						if (file.exists()) {
 							file.setContents(stream, false, true, subMonitor.split(1));
@@ -58,6 +71,51 @@ public class GenerateYamlGsnHandler extends AbstractHandler {
 			StatusManager.getManager().handle(status, StatusManager.LOG | StatusManager.SHOW);
 		} catch (InterruptedException e) {
 		}
+
+		/**
+		 * Dio: This is a temporary call to compile the yaml files.
+		 * It should be replaced by the proper one
+		 */
+		try {
+			// folderPath contains the path where all the yaml files are located
+			String osname = System.getProperty("os.name");
+
+			if (osname.equalsIgnoreCase("LINUX")) {
+				ProcessBuilder pb = new ProcessBuilder("sh", "-c", "gsn2x *.yaml");
+
+				String abspath = pathtoYaml;
+
+				pb.directory(new File(abspath));
+				Process process = pb.start();
+
+				StringBuilder output = new StringBuilder();
+				BufferedReader reader
+				= new BufferedReader(new InputStreamReader(
+						process.getInputStream()));
+
+				String line;
+				while ((line = reader.readLine()) != null) {
+					output.append(line + "\n");
+				}
+
+				int exitVal = process.waitFor();
+				if (exitVal == 0) {
+					System.out.println(" --- GSN2X SVG GENERATION PROCESS OUTPUT ---");
+					System.out.println(output);
+				} else {
+					System.out.println("gsn2x svg generation process error: "+exitVal);
+				}
+			} else {
+				System.out.println("Could not compile yaml files. Feature only enabled in Linux. Now running on:"+osname);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		/**
+		 * Dio: End of yaml -> svg generation
+		 */
+
 		return null;
 	}
 
