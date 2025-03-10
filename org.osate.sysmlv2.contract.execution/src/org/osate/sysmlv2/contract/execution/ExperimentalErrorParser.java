2c33a535d86a36644115c199333f4378861c190a
Aaron Greenhouse <aaron.greenhouse@gmail.com>
2025-03-10 16:49:03 -0400

    new project

---
+++ org.osate.sysmlv2.contract.execution/src/org/osate/sysmlv2/contract/execution/ExperimentalErrorParser.java
@@ -0,0 +1,103 @@
+package org.osate.sysmlv2.contract.execution;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class ExperimentalErrorParser {
+
+	public class ErrorPair {
+		public Element element;
+		public String error;
+
+		public ErrorPair(Element e, String err) {
+			element = e;
+			error = err;
+		}
+	}
+
+	public static ExperimentalErrorParser getParser() {
+		return new ExperimentalErrorParser();
+	}
+
+	PythonHelper phelper = PythonHelper.get();
+	AnalysisErrorReporterManager errManager = new AnalysisErrorReporterManager(
+			new MarkerAnalysisErrorReporter.Factory(AadlConstants.AADLOBJECTMARKER));
+
+
+	public InstanceObject getObjectInstanceFromId(int id) {
+		return phelper.getInstanceObjectMapper().getInstanceObject(id);
+	}
+
+	public void markErrors(String errString) {
+		ArrayList<ExperimentalErrorParser.ErrorPair> errors = parseErrors(null, errString);
+		for (ErrorPair pair : errors) {
+			errManager.error(pair.element, pair.error);
+		}
+	}
+
+	public void markUnfulfilledObjectives(String errString) {
+		ArrayList<ExperimentalErrorParser.ErrorPair> errors = parseErrors(null, errString);
+		for (ErrorPair pair : errors) {
+			errManager.error(pair.element, pair.error + " Unfulfilled. Not enough details to satisfy all assumptions");
+		}
+	}
+
+	public void markInfo(String errString) {
+		ArrayList<ExperimentalErrorParser.ErrorPair> errors = parseErrors(null, errString);
+		for (ErrorPair pair : errors) {
+			errManager.info(pair.element, pair.error);
+		}
+	}
+
+	public ArrayList<ErrorPair> parseErrors(Element root, String errString) {
+
+		ArrayList<ErrorPair> errors = new ArrayList<ErrorPair>();
+
+		if (errString.length() == 0) {
+			return errors;
+		}
+
+		if (errString.charAt(0) == '[') {
+			errString = errString.substring(1);
+		}
+
+		if (errString.charAt(errString.length() - 1) == ']') {
+			errString = errString.substring(0, errString.length() - 1);
+		}
+
+		StringTokenizer errorTokenizer = new StringTokenizer(errString, "|");
+		String contractName = "";
+		while (errorTokenizer.hasMoreTokens()) {
+			String token = errorTokenizer.nextToken();
+
+			token = token.trim();
+
+			if (token.length() == 0) {
+				continue;
+			}
+
+			// Skip the unique error number needed for unique z3 assert_and_track() call
+			if (token.charAt(0) == '#') {
+				continue;
+			}
+
+			// Skip any token with a leading comma
+			if (token.charAt(0) == ',') {
+				continue;
+			}
+
+			InstanceObject instance = (InstanceObject) root;
+			String errMessage = token;
+			if (token.contains("{")) {
+				String ref = token.substring(token.indexOf("{") + 1, token.indexOf("}"));
+				errMessage = token.substring(token.indexOf("}") + 1);
+				int index = Integer.parseInt(ref);
+				instance = getObjectInstanceFromId(index);
+			}
+			errors.add(new ErrorPair(instance, contractName + ":" + errMessage));
+		}
+
+		return errors;
+	}
+
+}
